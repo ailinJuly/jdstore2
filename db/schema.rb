d01a8bc160418d26c2ad242ab2ace1cc504876e6
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-06-13 21:13:33 +0800

    CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612133854) do
+ActiveRecord::Schema.define(version: 20170613122524) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "quantity"
+    t.integer  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
