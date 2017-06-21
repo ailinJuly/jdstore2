e2d87967b29bd9d5a05ffdab20cf4efed4649f31
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-06-21 20:05:55 +0800

    add ship/shipped/cancel/return action to admin/order_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   root 'products#index'
   namespace :admin do
     resources :products
-    resources :orders
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
   end
 
   resources :products do
@@ -33,5 +40,7 @@ Rails.application.routes.draw do
       post :pay_with_wechat
     end
   end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
